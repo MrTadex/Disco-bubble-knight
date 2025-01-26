20306326ed1d505b2f0fbe462b7e393ffae37088
Tadej Tomažin <tadexqx@gmail.com>
2025-01-26 15:19:20 +0100

    Menu

--- Scenes/Scripts/Menu.gd
+++ Scenes/Scripts/Menu.gd
@@ -3,8 +3,5 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Chapter1.tscn")
 
-func _on_option_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Options.tscn")
-
 func _on_exit_pressed() -> void:
 	get_tree().quit
